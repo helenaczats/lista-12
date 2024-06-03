6a0a645b7a34a78450dc7e3d5b53dbfdaa9a1165
helenaczats <helenaczats@gmail.com>
2024-06-03 21:49:46 +0200

    first commit

---
+++ resources/variables.py
@@ -0,0 +1,9 @@
+# Variaveis (globais, são usadas por todos)
+url = "https://restful-booker.herokuapp.com"             #endereço da API
+content_type = "application/json"                        # formato dos dados que serao transmitidos
+firstname = "Niko"
+lastname = "Pirosmani"
+totalprice = 120
+depositpaid = False
+bookingdates = dict(checkin="2024-06-27", checkout="2024-06-27")
+additionalneeds = "Breakfast"
